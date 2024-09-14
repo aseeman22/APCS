5d05767d2d9375c3beef74986a939daf2e547909
aseeman22 <anneseeman27@marlborough.org>
2024-09-14 11:10:59 -0700

    Multiples of 3

---
+++ MultiplesOf3.java
@@ -0,0 +1,13 @@
+
+public class Excercize1 {
+ 
+   public static void main(String[] args) {
+    for(int i = 0; i<31; i ++)
+    {
+    
+    }
+   
+      System.out.println("Hello World!");
+   }
+      
+}  
