d715bb5d35665e223ba32b66823da9e8957486de
aseeman22 <anneseeman27@marlborough.org>
2024-09-18 12:52:06 -0700

    work

--- unit1/MultiplesOf3.java
+++ unit1/MultiplesOf3.java
@@ -1,13 +1,14 @@
 
-public class Excercize1 {
+public class MultiplesOf3 {
  
-   public static void main(String[] args) {
-    for(int i = 0; i<31; i ++)
+   public static void main(String[] args) 
+   {
+    for(int i = 3; i<31; i ++)
     {
-    
+        if(i%3 == 0)
+        System.out.println(i);
     }
-   
-      System.out.println("Hello World!");
    }
       
 }  
+
