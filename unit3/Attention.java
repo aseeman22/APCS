a82ab1eac65b7c10d269f1c9ede71bb1ffdff1a0
aseeman22 <anneseeman27@marlborough.org>
2024-10-06 14:16:23 -0700

    work

---
+++ unit3/Attention.java
@@ -0,0 +1,35 @@
+public class Attention{
+  public static boolean attention(String attentionString){
+      String attentionSubstring;
+      attentionSubstring = attentionString.substring(0,9);
+      if(attentionSubstring.equals("Hey, You!"))
+      {
+          return true;
+      }
+      else 
+      {
+          return false;
+      }
+  }
+    
+    public static void testAttention(String attentionString, boolean expected){
+        boolean result = attention(attentionString);
+        
+        System.out.println("attention: " + attentionString + " expected: " + expected + " result " + result);
+            
+        if (result == expected)
+        {
+            System.out.println("YAY!");
+        } 
+        else
+        {
+            System.out.println("BOOHOO!");
+        }
+    }
+     public static void main(String[] args){
+         testAttention("Hey, You! I am happy.", true);
+         testAttention("Hey, I am happy.", false);
+         testAttention("I am happy.", false);
+     }
+         
+}
---
+++ unit3/DieRolling.java
@@ -0,0 +1,7 @@
+public class DieRolling{
+    public static int DieRolling(){
+        int Die;
+        Die = (int) (Math.random() * 5 + 1);
+        return Die;  
+    }
+}
