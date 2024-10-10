5e261ddf3f1100b2d71f34e571c7b09500efe844
aseeman22 <anneseeman27@marlborough.org>
2024-10-10 09:41:12 -0700

    work

--- unit3/DieRolling.java
+++ unit3/DieRolling.java
@@ -1,7 +1,13 @@
 public class DieRolling{
-    public static int DieRolling(){
+    public static int DieRoll(){
         int Die;
-        Die = (int) (Math.random() * 5 + 1);
+        Die = (int) (Math.random() * 6 + 1);
         return Die;  
     }
+  public static void main(String[] args){
+      for(int i = 1; i<100; i++)
+      {
+          System.out.println(DieRoll());
+      }
+  }
 }
--- unit3/hasTeen.java
+++ unit3/hasTeen.java
@@ -1,5 +1,7 @@
+
 public class hasTeen{
-    public boolean hasTeen(int a, int b, int c) {
+    
+    public static boolean hasTeen(int a, int b, int c) {
         if (a>= 13 && a<=19){
             return true;
             } 
@@ -14,4 +16,26 @@ public class hasTeen{
                 return false;
             }
         }
+ public static void testhasTeen(int a, int b, int c, boolean expected){
+     boolean result = hasTeen(a, b, c);
+   
+    System.out.println(" a: " + a + " b: " + b + " c: " + c + " expected: " + expected + " result: " + result);
+    
+    if (result == expected)
+    {
+        System.out.println("YAY!");
     }
+    else
+    {
+        System.out.println("BOOHOO!");
+    }
+}
+    public static void main(String[] args) 
+    {
+        testhasTeen(15, 16, 21, true);
+        testhasTeen(12, 7, 30, false);
+        testhasTeen(13, 18, 17, true);
+        testhasTeen(1, 3, 7, false);
+    }
+ }
+
