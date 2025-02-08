c41fa18be850f1036e81596a38fcb73913accac0
aseeman22 <anneseeman27@marlborough.org>
2025-02-07 17:29:16 -0800

    work

---
+++ unit10/PersonStats.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class PersonStats
+{
+    public PersonStats()
+    {
+        
+    }
+    
+    
+    public static int AverageNumberOfPets(ArrayList<Person> people)
+    {
+        int value = 0;
+        for (Person currentPerson:people)
+        {
+            value += currentPerson.getNumberOfPets();
+            value = value/people.size();
+            
+        }
+        return value;
+    }
+    public static String PersonWithMostPets(ArrayList<Person> people)
+    {
+        String name = " "; //store the name of person with greatest number of pets
+        int GreatestPets = 0; // store greatest number of pets seen so far
+        for(Person currentPerson: people)
+        {
+            if(currentPerson.getNumberOfPets()>GreatestPets)
+            {
+               GreatestPets = currentPerson.getNumberOfPets(); //update what the greatest amoount of pets is
+                name = currentPerson.getName(); // updating the name of person with greatest number of pets
+            }
+        }
+        return name;
+    }
+    public static ArrayList<Person> MoreThan2Pets(ArrayList<Person> InputPeople)
+    {
+        ArrayList<Person> AtLeast2Pets = new ArrayList<Person>();
+        for(Person currentPerson : InputPeople)
+        {
+            if(currentPerson.getNumberOfPets()>1)
+            {
+                AtLeast2Pets.add(currentPerson);
+            }
+        }
+        return AtLeast2Pets;
+        
+        
+    }
+}
--- unit10/PersonTest.java
+++ unit10/PersonTest.java
@@ -4,6 +4,7 @@ public class PersonTest
 {
     public static void main(String[] args)
     {
+        //testing PeopleStats.AverageNumberOfPets
         Person Annie = new Person("Annie", (int)4.0);
         Person fnsey = new Person("fnsey", (int)3.0);
         Person nndifn = new Person("nndifn", (int)2.0);
@@ -15,7 +16,7 @@ public class PersonTest
             people.add(nndifn);
         
         
-        double ExpectedAverageValue = 3;
+        double ExpectedAverageValue = 3.0;
         double ActualAverageValue = calculator.AverageNumberOfPets(people);
         
         if(ExpectedAverageValue == ActualAverageValue)
@@ -27,6 +28,7 @@ public class PersonTest
             System.out.println("NOOO!");
         }
         
+        //Testing PeopleStats.PersonWithMostPets
         Person diofj = new Person("diofj", (int)4);
         Person vnds = new Person("vnds", (int)3);
         Person fjned = new Person("fjned", (int)2);
@@ -48,23 +50,46 @@ public class PersonTest
             System.out.println("Noooo");
         }
         
-        Person dfiea = new person("dfiea", (int)4);
-        Person fneih = new person("fneih", (int)2);
-        Person njejij = new person("njejij", (int)1);
+        //testing PeopleStats.MoreThan2Pets
+        System.out.println("testing More than 2 pets");
+        Person dfiea = new Person("dfiea", (int)4);
+        Person fneih = new Person("fneih", (int)2);
+        Person njejij = new Person("njejij", (int)1);
         
-        ArrayList<String> People = new ArrayList<String>();
-            //People.add("Annie", 4);
-            //People.add("fneih", 2);
-           // People.add("njejij", 1);
+        ArrayList<Person> People = new ArrayList<Person>();
+            People.add(dfiea);
+            People.add(fneih);
+            People.add(njejij);
         
-        for(Person name: People)
+         ArrayList<Person> ExpectedArrayList = new ArrayList<Person>();
+            ExpectedArrayList.add(dfiea);
+            ExpectedArrayList.add(fneih);
+        
+        ArrayList<Person> FinalArrayList = PersonStats.MoreThan2Pets(People);
+        
+        for(Person randomPerson: FinalArrayList)
         {
-            if(name.getNumberOfPets()>1)
+           boolean HasBeenFound= false;
+            for(Person MaybeSamePerson: ExpectedArrayList)
             {
-                return People2.add(name); 
+                if(randomPerson.getNumberOfPets() == MaybeSamePerson.getNumberOfPets())
+                {
+                    if(randomPerson.getName()== MaybeSamePerson.getName())
+                    {
+                        HasBeenFound = true;
+                    }
+                }
+            }
+            if(HasBeenFound == false)
+            {
+                System.out.println("Noooo");
+            }
+            else
+            {
+                System.out.println("Yayy!");
             }
         }
-        
+          
         
         
     }
