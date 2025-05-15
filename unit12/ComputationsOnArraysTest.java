a5ba53045e50cc2fd3e7c5329541bc9c02a63a9c
aseeman22 <anneseeman27@marlborough.org>
2025-05-15 10:34:53 -0700

    work

--- unit12/ComputationsOnArraysTest.java
+++ unit12/ComputationsOnArraysTest.java
@@ -3,7 +3,19 @@ public class ComputationsOnArraysTest
      public static void main(String[] args)
   {
          //smallest integer test
+         int[][] numbers = {1 2 3 4
+                            5 6 7 8
+                            9 1 2 3}
          
-         int result = ComputationsOnArrays.SmallestInteger(numbers)
+         int[][] numbers = {3 5 1 8 6 2
+                            5 3 7 2 5 0}
+         
+         
+         ComputationsOnArrays.SmallestInteger(numbers);
+         
+         
+         System.out.print("expected:\n9\n8");
+         
+         
   }
 }
