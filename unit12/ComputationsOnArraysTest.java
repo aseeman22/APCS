2b89b571635af171f2d57fdb2a639d8247114f4e
aseeman22 <anneseeman27@marlborough.org>
2025-05-15 09:16:21 -0700

    work

---
+++ unit12/ComputationsOnArraysTest.java
@@ -0,0 +1,9 @@
+public class ComputationsOnArraysTest
+{
+     public static void main(String[] args)
+  {
+         //smallest integer test
+         
+         int result = ComputationsOnArrays.SmallestInteger(numbers)
+  }
+}
